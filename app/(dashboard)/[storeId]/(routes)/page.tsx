ee178f3273f8e7aa385122e4f1b1796dacdc62f2
egemenozyurek <ozyurekegemen95@gmail.com>
2023-11-05 16:50:54 +0000

    store switcher on navbar

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,17 @@
-const DashboardPage = () => {
-    return (
-        <div>this is a dashboard</div>
-    )
+import prismadb from "@/lib/prismadb";
+import React from "react";
+
+interface DashboardPageProps {
+  params: { storeId: string };
 }
 
-export default DashboardPage
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active Store: {store?.name}</div>;
+};
+
+export default DashboardPage;
